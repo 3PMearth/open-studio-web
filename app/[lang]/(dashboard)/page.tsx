e9ce6a4a32377108fcb9bee82e8eb0601483dfc4
jisoo <111257087+jisoo-3pm@users.noreply.github.com>
2023-07-19 16:38:08 +0900

    feat: localize client component with next-intl

--- app/[lang]/(dashboard)/page.tsx
+++ app/[lang]/(dashboard)/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import { useTranslations } from "next-intl";
+
 import withAuth from "../components/auth/withAuth";
 
 function Home() {
+  const t = useTranslations("home");
+
   return (
     <div>
-      <p>welcome</p>
+      <p>{t("welcome")}</p>
     </div>
   );
 }
---
+++ get-messages.ts
@@ -0,0 +1,11 @@
+import "server-only";
+import type { Locale } from "./i18n-config";
+
+// We enumerate all dictionaries here for better linting and typescript support
+// We also get the default import for cleaner types
+const messages = {
+  en: () => import("./messages/en.json").then(module => module.default),
+  ko: () => import("./messages/ko.json").then(module => module.default)
+};
+
+export const getMessages = async (locale: Locale) => messages[locale]();
