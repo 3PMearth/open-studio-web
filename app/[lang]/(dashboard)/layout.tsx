d5f5001e99e3b066531970fb5763069cd55b2cb1
jisoo <111257087+jisoo-3pm@users.noreply.github.com>
2023-07-19 15:18:14 +0900

    feat: add route group - dashboard

---
+++ app/[lang]/(dashboard)/layout.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { Dialog, Transition } from "@headlessui/react";
+import { useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+
+import AuthProvider from "../components/auth/AuthProvider";
+import Sidebar from "../components/layout/Sidebar";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [isSidebarShowing, setIsSidebarShowing] = useState(false);
+
+  return (
+    <AuthProvider>
+      <div className="relative flex min-h-screen">
+        <Dialog
+          as="div"
+          className="fixed inset-y-0 z-40 md:hidden"
+          open={isSidebarShowing}
+          onClose={() => setIsSidebarShowing(false)}
+        >
+          <Transition
+            as="div"
+            className="absolute inset-y-0"
+            show={isSidebarShowing}
+            enter="transition-opacity duration-75"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity duration-150"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Sidebar />
+          </Transition>
+        </Dialog>
+        <div className="hidden md:block">
+          <Sidebar />
+        </div>
+        <div className="flex flex-1 flex-col">
+          <header className="flex h-12 items-center border-b border-[#d9d9d9] bg-white px-6 md:px-10">
+            <button
+              id="menu"
+              className="mr-4 md:hidden"
+              onClick={() => setIsSidebarShowing((prev) => !prev)}
+            >
+              <AiOutlineMenu color="black" />
+            </button>
+            <div className="flex-1 text-[#667085]">Breadcrumb</div>
+          </header>
+          <main className="flex-1 bg-gray-background p-6 text-black md:p-10">
+            {children}
+          </main>
+        </div>
+      </div>
+    </AuthProvider>
+  );
+}
---
+++ app/[lang]/components/auth/index.ts
@@ -0,0 +1,2 @@
+export { default } from "./AuthProvider";
+export { default } from "./withAuth";
