e2aa0c68569ce9c4b85a4deb16e5f96b4d443a42
jisoo <111257087+jisoo-3pm@users.noreply.github.com>
2023-07-21 20:21:29 +0900

    style: token item

--- components/token-item/TokenItem.tsx
+++ components/token-item/TokenItem.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 import { Token } from "types/token";
 
 interface TokenItemProps {
@@ -5,5 +8,27 @@ interface TokenItemProps {
 }
 
 export default function TokenItem({ token }: TokenItemProps) {
-  return <div>{token.name}</div>;
+  const { token_img, name } = token;
+
+  return (
+    <Link href="/todo">
+      <div>
+        <div className="aspect-square bg-gray-semilight">
+          {token_img && (
+            <Image
+              layout="responsive"
+              src={token_img}
+              alt={name}
+              width={135}
+              height={135}
+            />
+          )}
+        </div>
+        <div className="flex h-20 flex-col justify-between px-3 py-[0.7rem]">
+          <p>{token.name}</p>
+          <p className="text-xs">See More {">"}</p>
+        </div>
+      </div>
+    </Link>
+  );
 }
