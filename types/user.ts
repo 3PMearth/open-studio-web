0c54c26f5ecf75f836ee8442c272ae8fd7c9b46f
jisoo <111257087+jisoo-3pm@users.noreply.github.com>
2023-07-21 17:48:13 +0900

    feat: store user data on session storage

---
+++ types/user.ts
@@ -0,0 +1,10 @@
+export type User = {
+  id: string;
+  wallet_address: string;
+  email?: string;
+  phone_number?: string;
+  country_code?: string;
+  first_name?: string;
+  last_name?: string;
+  sso_type: string;
+};
