543ceb1ebec599dbf3f558fd8ebf9158a511f1df
jisoo <111257087+jisoo-3pm@users.noreply.github.com>
2023-09-07 11:03:16 +0900

    feat(order): add success, error page

--- app/[lang]/(shop)/order/success/page.tsx
+++ app/[lang]/(shop)/order/success/page.tsx
@@ -1,5 +1,57 @@
+"use client";
+
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import { useTranslations } from "next-intl";
+import { useEffect, useState } from "react";
+
+import { getToken } from "api";
+import Button from "components/button";
+import TokenThumbnail from "components/token-thumbnail";
+import type { Token } from "types/token";
+
 function OrderSuccess() {
-  return <>success</>;
+  const t = useTranslations("payment");
+
+  const [token, setToken] = useState<Token>();
+
+  const searchParams = useSearchParams();
+  const tokenId = searchParams.get("tokenId");
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      const _token = await getToken(tokenId as string);
+      if (_token?.id) {
+        setToken(_token);
+      }
+    };
+
+    if (tokenId) {
+      fetchToken();
+    }
+  }, [tokenId]);
+
+  return (
+    <div className="p-4 lg:p-6">
+      <h1 className="text-2xl font-semibold leading-8">{t("thankYouTitle")}</h1>
+      <div className="text-center">
+        {token?.token_img && (
+          <div className="mx-auto mt-8 h-60 w-60">
+            <TokenThumbnail alt={token?.name} imgUrl={token?.token_img} />
+          </div>
+        )}
+        <p className="mt-2 text-lg font-semibold">{token?.name}</p>
+        <Link className="mt-8 inline-block" href={`/my`}>
+          <Button size="large">{t("goToMyPage")}</Button>
+        </Link>
+      </div>
+      <p className="mt-6 break-keep text-center text-sm">
+        {t("willBeTransfered")}
+        <br />
+        {t("willBeRefunded")}
+      </p>
+    </div>
+  );
 }
 
 export default OrderSuccess;
